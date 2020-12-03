dfc80185c3a4905899acdb46a6780ac976caebbb
Jason M Graham <prof.jason.m.graham@gmail.com>
2020-12-03 08:44:41 -0500

    update

--- nntutorial.jl
+++ nntutorial.jl
@@ -1,10 +1,10 @@
 module nntutorial
 
-using LinearAlgebra, DataFrames;
+using LinearAlgebra, DataFrames, Plots, StatsPlots, LaTeXStrings;
 
-export f, simpleloopednncal, matrixnncal, graddesc
+export factivate, simpleloopednncal, matrixnncal, graddescDF, graddescsim
 
-f(x) = 1.0/(1.0 + exp(-x));
+factivate(x) = 1.0/(1.0 + exp(-x));
 
 function simpleloopednncal(n_layers,x,w,b)
     h = zeros(length(w));
@@ -21,7 +21,7 @@ function simpleloopednncal(n_layers,x,w,b)
                  f_sum = f_sum + w[l][i,j]*node_in[j];
             end
             f_sum = f_sum + b[l][i];
-            h[i] = f(f_sum); 
+            h[i] = factivate(f_sum); 
         end
     end
     return h
@@ -36,12 +36,12 @@ function matrixnncal(n_layers,x,w,b)
             node_in = h;
         end
         z = w[l]*node_in .+ b[l];
-        h = f.(z);
+        h = factivate.(z);
     end
     return h
 end
 
-function graddesc(x,α,j)
+function graddescDF(f,f_cost,x,α,j)
     xtrace = zeros(j);
     xtrace[1] = x;
     ftrace = zeros(j);
@@ -52,6 +52,16 @@ function graddesc(x,α,j)
     end
     DF = DataFrame(x=xtrace,f_x=ftrace)
     return DF
+end
+
+function graddescsim(x_vals,f,f_cost,x,α,j,anim_name;fps_val=10)
+    y_vals = [f(x) for x in x_vals];
+    DF = graddescDF(f,f_cost,x,α,j);
+    p = plot(x_vals,y_vals,legend=false,title=L"\alpha = %$α");
+    anim = @animate for i ∈ 1:nrow(DF)
+        plot!(p,DF[1:i,1],DF[1:i,2],m=:dot);
+    end
+    gif(anim, anim_name, fps = fps_val);
 end
 
 end
